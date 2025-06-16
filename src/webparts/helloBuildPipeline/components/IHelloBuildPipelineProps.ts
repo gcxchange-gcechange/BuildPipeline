27930f27f96180e2babe6bd01a4bf283f07a89ac
Adi Makkar <adimakkar.ca@gmail.com>
2025-06-16 11:46:15 -0400

    first commit

---
+++ src/webparts/helloBuildPipeline/components/IHelloBuildPipelineProps.ts
@@ -0,0 +1,7 @@
+export interface IHelloBuildPipelineProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
